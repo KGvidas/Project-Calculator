05e4bfad8391c89b9474a19fadb072d4e0302f68
KGvidas <122453305+KGvidas@users.noreply.github.com>
2024-08-29 17:01:11 +0300

    Add basic functions and func Operate

---
+++ js.js
@@ -0,0 +1,41 @@
+function add(aNumber, bNumber){
+    return aNumber + bNumber;
+}
+function subtract(aNumber, bNumber){
+    return aNumber - bNumber;
+}
+function multiply(aNumber, bNumber){
+    return aNumber * bNumber;
+}
+function divide(aNumber, bNumber){
+    return aNumber / bNumber;
+}
+
+let aNumber;
+let bNumber;
+let operator;
+
+function operate(aNumber, bNumber, operator){
+   switch (operator) {
+    case add:
+        return add(aNumber, bNumber)
+        break;
+
+    case subtract:
+        return subtract(aNumber, bNumber)
+        break;
+        
+    case multiply:
+        return multiply(aNumber, bNumber)
+        break;
+    
+    case divide:
+        return divide(aNumber, bNumber)
+        break;
+   
+    default:
+        break;
+   }
+
+}
+
